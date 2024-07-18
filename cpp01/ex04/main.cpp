e2fb1d8cdeda5550c07395938ab7457a32f1d695
Enzo Degraeve <enzodeg40@gmail.com>
2024-07-18 13:02:45 +0200

    Ex04

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,20 +6,55 @@
 /*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 11:35:25 by edegraev          #+#    #+#             */
-/*   Updated: 2024/07/09 11:38:59 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/07/18 13:01:41 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fstream>
 #include <iostream>
+
+void find_and_replace(std::string& data, const std::string& search, const std::string& replace)
+{
+	size_t pos = 0;
+	while ((pos = data.find(search, pos)) != std::string::npos)
+	{
+		data.erase(pos, search.length());
+		data.insert(pos, replace);
+		pos += replace.length();
+	}
+}
+
+void write_file(std::string filename, std::string data)
+{
+	std::ofstream outfile;
+	
+	outfile.open(std::string(filename + ".replace").c_str());
+	outfile << data;
+	outfile.close();
+}
 
 int	main(int argc, char **argv)
 {
-    (void)argv;
-    if (argc != 4)
-    {
-        std::cout << "sed file string1 string2" << std::endl;
-        return 1;
-    }
-
-    return 0;
+	char c;
+	std::ifstream infile;
+	std::string data;
+	
+	(void)argv;
+	if (argc != 4)
+	{
+		std::cerr << "sed file string1 string2" << std::endl;
+		return 1;
+	}
+	infile.open(argv[1]);
+	if (infile.fail())
+	{
+		std::cerr << "Error: " << argv[1] << ": no such file or directory" << std::endl;
+		return 1;
+	}
+	while(infile >> std::noskipws >> c && !infile.eof())
+		data += c;
+	infile.close();
+	find_and_replace(data, argv[2], argv[3]);
+	write_file(argv[1], data);
+	return 0;
 }
