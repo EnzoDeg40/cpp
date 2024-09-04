ff680577faa324ce327296db43865d939f2b8408
Enzo Degraeve <enzodeg40@gmail.com>
2024-09-04 10:49:50 +0200

    Additional output to cpp01 ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 21:32:40 by edegraev          #+#    #+#             */
-/*   Updated: 2024/07/08 21:34:42 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/09/04 10:43:56 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@ int main(void)
     std::cout << "Address of mystring: " << &mystring << std::endl;
     std::cout << "Address of stringPTR: " << stringPTR << std::endl;
     std::cout << "Address of stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Value of mystring: " << mystring << std::endl;
+    std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value of stringREF: " << stringREF << std::endl;
 
     return (0);
 }
