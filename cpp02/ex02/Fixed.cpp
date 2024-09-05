861e787bf514e169ffdf9e9aa9e9cacd9df356fb
Enzo Degraeve <enzodeg40@gmail.com>
2024-09-05 14:16:32 +0200

    Import main test and remove printf

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 11:00:20 by edegraev          #+#    #+#             */
-/*   Updated: 2024/09/05 09:20:39 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/09/05 14:12:13 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 // Constructors
 Fixed::Fixed() : _fixed_point_value(0)
 {
-	std::cout << "Default constructor called" << std::endl;
+
 }
 
 Fixed::Fixed(const Fixed &other)
@@ -26,26 +26,23 @@ Fixed::Fixed(const Fixed &other)
 
 Fixed::Fixed(const int value)
 {
-	std::cout << "Int constructor called" << std::endl;
 	_fixed_point_value = value << _n_fract_bits;
 }
 
 Fixed::Fixed(const float value)
 {
-	std::cout << "Float constructor called" << std::endl;
 	_fixed_point_value = roundf(value * (1 << _n_fract_bits));
 }
 
 // Destructor
 Fixed::~Fixed()
 {
-	std::cout << "Destructor called" << std::endl;
+    
 }
 
 // Overloaded Operators
 Fixed & Fixed::operator = (Fixed const & rhs)
 {
-	// std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &rhs)
         _fixed_point_value = rhs.getRawBits();
     return (*this);
