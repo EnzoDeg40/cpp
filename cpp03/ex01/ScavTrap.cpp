53bf8d14208ff5ccdadee5f5341f433c7721d36a
Enzo Degraeve <enzodeg40@gmail.com>
2024-09-10 17:05:42 +0200

    Create ScavTrap

--- cpp03/ex01/ScavTrap.cpp
+++ cpp03/ex01/ScavTrap.cpp
@@ -6,29 +6,41 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 09:09:38 by edegraev          #+#    #+#             */
-/*   Updated: 2024/09/09 15:48:38 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/09/10 16:28:18 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 
+// Constructeur par défaut
 ScavTrap::ScavTrap(std::string name) : _hitPoints(100), _energyPoints(50), _attackDamage(20), _name(name)
 {
-	// todo remove
-	(void)_hitPoints;
-	(void)_energyPoints;
-	(void)_attackDamage;
+    // todo remove
+    (void)_hitPoints;
+    (void)_energyPoints;
+    (void)_attackDamage;
 }
 
-// Recopie 
-ScavTrap::ScavTrap(const ScavTrap& other)
+// Affectation
+ScavTrap &ScavTrap::operator=(const ScavTrap &other)
 {
-	_hitPoints = other._hitPoints;
-	_energyPoints = other._energyPoints;
-	_attackDamage = other._attackDamage;
-	_name = other._name;
+    if (this != &other)
+    {
+        _hitPoints = other._hitPoints;
+        _energyPoints = other._energyPoints;
+        _attackDamage = other._attackDamage;
+        _name = other._name;
+    }
+    return *this;
 }
 
+// Recopie
+ScavTrap::ScavTrap(const ScavTrap &other)
+{
+    *this = other;
+}
+
+// Destructor
 ScavTrap::~ScavTrap()
 {
 }
--- cpp03/ex01/ScavTrap.hpp
+++ cpp03/ex01/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 09:09:41 by edegraev          #+#    #+#             */
-/*   Updated: 2024/09/09 09:24:58 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/09/10 16:28:53 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,12 @@ private:
 	std::string _name;
 
 public:
+    // Default constructor
 	ScavTrap(std::string name);
+    // Copy constructor
+    ScavTrap(const ScavTrap &other);
+    // Assignment operator
+    ScavTrap &operator=(const ScavTrap &other);
+    // Destructor
 	~ScavTrap();
 };
