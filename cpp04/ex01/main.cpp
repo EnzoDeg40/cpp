a1e1d393f4c8b983b2a02b9505ee0f88d1bcd137
Enzo Degraeve <enzodeg40@gmail.com>
2024-09-23 10:50:44 +0200

    Add main from subject

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 09:28:46 by edegraev          #+#    #+#             */
-/*   Updated: 2024/09/23 10:32:56 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/09/23 10:49:17 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,21 @@ int main()
 
 		delete cat;
 		delete dog;
+		std::cout << "=== End of test\n" << std::endl;
+	}
+	{
+		std::cout << "=== Subject Test" << std::endl;
+		
+		const Animal* meta[10];
+
+		for (int i = 0; i < 5; i++)
+			meta[i] = new Dog();
+		for (int i = 5; i < 10; i++)
+			meta[i] = new Cat();
+
+		for (int i = 0; i < 10; i++)
+			delete meta[i];
+
 		std::cout << "=== End of test\n" << std::endl;
 	}
 }
