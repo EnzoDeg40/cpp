5fec787fc51e95ec3a8d5153872f5e38002b1b87
Enzo Degraeve <enzodeg40@gmail.com>
2024-09-23 21:00:16 +0200

    Restart

---
+++ cpp03bis/ex00/ClapTrap.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 14:43:37 by edegraev          #+#    #+#             */
+/*   Updated: 2024/09/23 20:52:42 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name) : _name(name), _hp(10), _energy(10), _attack_damage(0)
+{
+    std::cout << "ClapTrap constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap()
+{
+    std::cout << "ClapTrap destructor called" << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+    std::cout << "ClapTrap assignation operator called" << std::endl;
+    if (this != &other)
+    {
+        this->_name = other._name;
+        this->_hp = other._hp;
+        this->_energy = other._energy;
+        this->_attack_damage = other._attack_damage;
+    }
+    return *this;
+}
+
+ClapTrap::ClapTrap(const ClapTrap& other)
+{
+    std::cout << "ClapTrap copy constructor called" << std::endl;
+    *this = other;
+}
