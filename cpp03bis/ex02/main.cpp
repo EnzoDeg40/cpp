c9a477929898f5527dadf84a441ba0f65a5277a6
Enzo Degraeve <enzodeg40@gmail.com>
2024-09-25 10:30:30 +0200

    Create fragtrap

--- cpp03bis/ex02/main.cpp
+++ cpp03bis/ex02/main.cpp
@@ -6,12 +6,13 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 20:53:12 by edegraev          #+#    #+#             */
-/*   Updated: 2024/09/25 10:01:51 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/09/25 10:27:42 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main()
 {
@@ -19,14 +20,17 @@ int main()
     // ClapTrap claptrap2(claptrap);
     // ClapTrap claptrap3 = claptrap;
 
-    ScavTrap scavtrap("Hugo");
-    scavtrap.takeDamage(1000);
-    scavtrap.attack("Enzo");
-    scavtrap.guardGate();
+    // ScavTrap scavtrap("Hugo");
+    // scavtrap.takeDamage(1000);
+    // scavtrap.attack("Enzo");
+    // scavtrap.guardGate();
 
-    ScavTrap scavtrap2("idk");
-    scavtrap2 = scavtrap;
-    scavtrap2.guardGate();
+    // ScavTrap scavtrap2("idk");
+    // scavtrap2 = scavtrap;
+    // scavtrap2.guardGate();
+
+    FragTrap fragtrap("Anthony");
+    fragtrap.highFivesGuys();
 
     return 0;
 }
