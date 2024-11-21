6709aa031c51f8dcdeb082ace88f3b8c684cc9c9
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-21 14:26:29 +0100

    Create RobotomyRequestForm

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,22 +6,25 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:24:27 by edegraev          #+#    #+#             */
-/*   Updated: 2024/11/19 11:39:25 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/11/21 14:24:51 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 int main()
 {
     Bureaucrat *bureaucrat = new Bureaucrat("Enzo", 1);
     (void)bureaucrat;
 
-    ShrubberyCreationForm shrubberyCreationForm("test");
-
+    ShrubberyCreationForm shrubberyCreationForm("test1");
+    RobotomyRequestForm robotomyRequestForm("test2");
+    
     bureaucrat->signForm(shrubberyCreationForm);
     shrubberyCreationForm.execute(*bureaucrat);
+    robotomyRequestForm.execute(*bureaucrat);
 
 	return 0;
 }
