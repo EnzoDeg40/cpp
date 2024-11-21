57711024b5c5933019539c3458d22e202d0d6209
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-21 14:34:35 +0100

    Create presidentialPardonForm

--- cpp05/ex02/main.cpp
+++ cpp05/ex02/main.cpp
@@ -6,13 +6,15 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:24:27 by edegraev          #+#    #+#             */
-/*   Updated: 2024/11/21 14:24:51 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/11/21 14:34:15 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main()
 {
@@ -21,10 +23,15 @@ int main()
 
     ShrubberyCreationForm shrubberyCreationForm("test1");
     RobotomyRequestForm robotomyRequestForm("test2");
-    
+    PresidentialPardonForm presidentialPardonForm("test3");
+        
     bureaucrat->signForm(shrubberyCreationForm);
+    bureaucrat->signForm(robotomyRequestForm);
+    bureaucrat->signForm(presidentialPardonForm);
+    
     shrubberyCreationForm.execute(*bureaucrat);
     robotomyRequestForm.execute(*bureaucrat);
-
+    presidentialPardonForm.execute(*bureaucrat);
+    
 	return 0;
 }
