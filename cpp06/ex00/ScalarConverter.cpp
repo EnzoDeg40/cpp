5d1721d0e8717104e23ff0034081dd8cb8296cbd
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-25 09:29:09 +0100

    Create cpp06 with ScalarConverter class

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 09:03:47 by edegraev          #+#    #+#             */
+/*   Updated: 2024/11/25 09:22:21 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
+
+void ScalarConverter::convert(const std::string str)
+{
+    (void)str;
+    std::cout << "Hello" << std::endl;
+}
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 09:04:40 by edegraev          #+#    #+#             */
+/*   Updated: 2024/11/25 09:15:59 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string.h>
+
+class ScalarConverter
+{
+public:
+    static void convert(const std::string str);
+
+private:
+    ScalarConverter(/* args */);
+    ~ScalarConverter();
+};
+
+
