62b0bff66232ce4e4290a27e781002b969251d31
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-27 09:12:15 +0100

    Move intern fn to cpp file

--- cpp05/ex03/Intern.cpp
+++ cpp05/ex03/Intern.cpp
@@ -3,10 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   Intern.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
+/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 10:56:44 by edegraev          #+#    #+#             */
-/*   Updated: 2024/11/22 10:56:45 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/11/27 09:09:48 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+
+Intern::Intern()
+{
+}
+
+Intern::~Intern()
+{
+}
+
+Intern::Intern(const Intern& copy)
+{
+    (void)copy;
+}
+
+Intern& Intern::operator=(const Intern& copy)
+{
+    (void)copy;
+    return (*this);
+}
+
+AForm* Intern::makeForm(std::string const name, std::string const target)
+{
+    AForm* forms[3] = {
+        new ShrubberyCreationForm(target),
+        new RobotomyRequestForm(target),
+        new PresidentialPardonForm(target)
+    };
+    
+    std::string names[3] = {
+        "shrubbery creation",
+        "robotomy request",
+        "presidential pardon"
+    };
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (name == names[i])
+        {
+            std::cout << "Intern creates " << name << std::endl;
+            for (int j = 0; j < 3; j++)
+            {
+                if (j != i)
+                    delete forms[j];
+            }
+            return forms[i];
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+        delete forms[i];
+    throw Intern::FormNotFoundException();
+}
+
+const char *Intern::FormNotFoundException::what() const throw()
+{
+    return "Error: form not found.";
+}
--- cpp05/ex03/Intern.hpp
+++ cpp05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 16:13:08 by edegraev          #+#    #+#             */
-/*   Updated: 2024/11/27 09:07:00 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/11/27 09:09:45 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@
 class Intern
 {
 private:
-    /* data */
 public:
     Intern();
     ~Intern();
+    Intern(const Intern& copy);
+    Intern& operator=(const Intern& copy);
 
     AForm* makeForm(std::string const name, std::string const target);
 
@@ -35,48 +36,4 @@ public:
     };
 };
 
-Intern::Intern()
-{
-}
 
-Intern::~Intern()
-{
-}
-
-AForm* Intern::makeForm(std::string const name, std::string const target)
-{
-    AForm* forms[3] = {
-        new ShrubberyCreationForm(target),
-        new RobotomyRequestForm(target),
-        new PresidentialPardonForm(target)
-    };
-    
-    std::string names[3] = {
-        "shrubbery creation",
-        "robotomy request",
-        "presidential pardon"
-    };
-
-    for (int i = 0; i < 3; i++)
-    {
-        if (name == names[i])
-        {
-            std::cout << "Intern creates " << name << std::endl;
-            for (int j = 0; j < 3; j++)
-            {
-                if (j != i)
-                    delete forms[j];
-            }
-            return forms[i];
-        }
-    }
-
-    for (int i = 0; i < 3; i++)
-        delete forms[i];
-    throw Intern::FormNotFoundException();
-}
-
-const char *Intern::FormNotFoundException::what() const throw()
-{
-    return "Error: form not found.";
-}
