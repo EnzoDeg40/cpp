c137b3828f3618287c72bc61dc2ea9eba43b17dc
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-28 15:56:12 +0100

    Implement serialize and deserialize methods in Serializer class

--- cpp06/ex01/Serializer.cpp
+++ cpp06/ex01/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 15:14:47 by edegraev          #+#    #+#             */
-/*   Updated: 2024/11/28 15:43:10 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/11/28 15:51:25 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,14 @@ Serializer::Serializer()
 
 Serializer::~Serializer()
 {
+}
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data*>(raw);
 }
