176da4e9a4a86ef0d55584f7726c20457210e199
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-29 21:55:18 +0100

    Update .gitignore and enhance iter function overloads for const reference support

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
+/*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 11:17:03 by edegraev          #+#    #+#             */
-/*   Updated: 2024/11/29 15:10:41 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/11/29 18:41:24 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 
 template <typename T>
 void iter(T* array, size_t size, void (*f)(T&))
+{
+    for (size_t i = 0; i < size; i++)
+        f(array[i]);
+}
+
+template <typename T>
+void iter(T* array, size_t size, void (*f)(T const&))
 {
     for (size_t i = 0; i < size; i++)
         f(array[i]);
