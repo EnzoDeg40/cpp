50d94508ce9af4e2f8927387cb5b065fd923475a
Enzo Degraeve <enzodeg40@gmail.com>
2024-11-30 10:08:01 +0100

    Ex02 cpp07

---
+++ cpp07/ex02/Array.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 09:27:54 by edegraev          #+#    #+#             */
+/*   Updated: 2024/11/30 09:58:43 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+private:
+    /* data */
+public:
+    Array(/* args */);
+    ~Array();
+};
+
+#include "Array.tpp"
