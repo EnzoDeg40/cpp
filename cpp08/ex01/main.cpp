7629869c77ace212dab2602983352cec40d269d1
Enzo Degraeve <enzodeg40@gmail.com>
2024-12-02 20:54:31 +0100

    Format like main

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 14:54:37 by edegraev          #+#    #+#             */
-/*   Updated: 2024/12/02 20:53:07 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/12/02 20:54:14 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int main()
 {
     Span sp = Span(5);
     
-    sp.addNumber(1);
-    sp.addNumber(2);
+    sp.addNumber(6);
     sp.addNumber(3);
-    sp.addNumber(4);
-    sp.addNumber(5);
+    sp.addNumber(17);
+    sp.addNumber(9);
+    sp.addNumber(11);
 
-    std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-    std::cout << "Longest span: " << sp.longestSpan() << std::endl;
-
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+    
     return (0);
 }
