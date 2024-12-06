b9c918b15a16d4c8acb53aed0eef37762908716d
Enzo Degraeve <enzodeg40@gmail.com>
2024-12-06 15:33:38 +0100

    Clean code

--- cpp09/ex01/RPN.cpp
+++ cpp09/ex01/RPN.cpp
@@ -6,24 +6,18 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 13:36:00 by edegraev          #+#    #+#             */
-/*   Updated: 2024/12/06 15:16:12 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/12/06 15:32:57 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-void RPN::parsing(const std::string& av)
+void RPN::processToken(const std::string& token, std::stack<int>& tokens)
 {
-    std::stack<int> tokens; // Pile d'entiers pour simplifier
-    std::istringstream iss(av);
-    std::string token;
-
-    while (iss >> token)
-    {
-        if (std::isdigit(token[0]) || (token[0] == '-' && token.size() > 1))
+    if (std::isdigit(token[0]) || (token[0] == '-' && token.size() > 1))
         {
             tokens.push(std::atoi(token.c_str()));
-            continue;
+            return;
         }
 
         if (token == "+" || token == "-" || token == "*" || token == "/")
@@ -33,32 +27,35 @@ void RPN::parsing(const std::string& av)
                 throw std::invalid_argument("Invalid expression: not enough operands");
             }
 
-            // Récupère les deux derniers opérandes
             int b = tokens.top(); tokens.pop();
             int a = tokens.top(); tokens.pop();
 
-            // Applique l'opération
             if (token == "+") tokens.push(a + b);
             else if (token == "-") tokens.push(a - b);
             else if (token == "*") tokens.push(a * b);
             else if (token == "/")
             {
                 if (b == 0)
-                {
                     throw std::runtime_error("Division by zero");
-                }
                 tokens.push(a / b);
             }
-            continue;
+            return;
         }
 
         throw std::invalid_argument("Invalid token: " + token);
-    }
+}
+
+void RPN::parsing(const std::string& av)
+{
+    std::stack<int> tokens;
+    std::istringstream iss(av);
+    std::string token;
+
+    while (iss >> token)
+        processToken(token, tokens);
 
     if (tokens.size() != 1)
-    {
         throw std::invalid_argument("Invalid expression: unbalanced tokens");
-    }
 
     std::cout << "Result: " << tokens.top() << std::endl;
 }
--- cpp09/ex01/RPN.hpp
+++ cpp09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 13:29:09 by edegraev          #+#    #+#             */
-/*   Updated: 2024/12/06 15:16:15 by edegraev         ###   ########.fr       */
+/*   Updated: 2024/12/06 15:32:41 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,8 @@ public:
     RPN(RPN const &src);
     RPN &operator=(RPN const &rhs);
 
-    // void parsing(std::string av);
+    
     void parsing(const std::string& av);
+    void processToken(const std::string& token, std::stack<int>& tokens);
 
 };
