04873a8926f65a52f3fb855f2240084fe4377436
Enzo Degraeve <enzodeg40@gmail.com>
2025-01-03 00:27:15 +0100

    Create PmergeMe class

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,27 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 00:25:51 by edegraev          #+#    #+#             */
-/*   Updated: 2025/01/03 00:26:51 by edegraev         ###   ########.fr       */
+/*   Updated: 2025/01/03 00:26:45 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
-
-class PmergeMe
-{
-private:
-    
-public:
-    PmergeMe();
-    ~PmergeMe();
-    PmergeMe(const PmergeMe &copy);
-    PmergeMe &operator=(const PmergeMe &copy);
-};
+#include "PmergeMe.hpp"
 
 PmergeMe::PmergeMe()
 {
