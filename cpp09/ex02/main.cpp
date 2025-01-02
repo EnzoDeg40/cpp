42a587bdafec28cbbbcbc1abd69ddc85fdd9f531
Enzo Degraeve <enzodeg40@gmail.com>
2025-01-03 00:23:41 +0100

    Refactor sorting algorithms and update main array for testing

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: edegraev <edegraev@student.forty2.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 09:52:05 by edegraev          #+#    #+#             */
-/*   Updated: 2025/01/03 00:10:30 by edegraev         ###   ########.fr       */
+/*   Updated: 2025/01/03 00:23:30 by edegraev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,87 @@ void print(std::vector<int> &list, std::string message = "List : ")
 
 void insertSort(std::vector<int> &list, int left, int right)
 {
-    (void)left;
-    (void)right;
-    (void)list;
+    // sort the list from left to right
+    for (int i = left + 1; i <= right; i++)
+    {
+        int key = list[i];
+        int j = i - 1;
 
-    print(list, "Insert list : ");
+        // move elements most bigger than key to the right
+        while (j >= 0 && list[j] > key)
+        {
+            list[j + 1] = list[j];
+            j = j - 1;
+        }
+
+        // insert key in the right place
+        list[j + 1] = key;
+    }
 }
 
 void merge(std::vector<int> &list, int left, int middle, int right)
 {
-    (void)list;
-    (void)left;
-    (void)middle;
-    (void)right;
+    int number1 = middle - left + 1;
+    int number2 = right - middle;
+
+    std::vector<int> left_array(number1);
+    std::vector<int> right_array(number2);
+
+    // copy numbers from list to left_array and right_array
+    for (int i = 0; i < number1; i++)
+        left_array[i] = list[left + i];
+    for (int i = 0; i < number2; i++)
+        right_array[i] = list[middle + 1 + i];
+
+    int i = 0;
+    int j = 0;
+    int k = left;
     
-    print(list, "Merge list : ");
+    // merge (and sort) the two arrays
+    while (i < number1 && j < number2)
+    {
+        if (left_array[i] <= right_array[j])
+        {
+            list[k] = left_array[i];
+            i++;
+        }
+        else
+        {
+            list[k] = right_array[j];
+            j++;
+        }
+        k++;
+    }
+
+    // copy the remaining elements of left_array
+    while (i < number1)
+    {
+        list[k] = left_array[i];
+        i++;
+        k++;
+    }
+
+    // copy the remaining elements of right_array
+    while (j < number2)
+    {
+        list[k] = right_array[j];
+        j++;
+        k++;
+    }
 }
 
 void mergeSort(std::vector<int> &list, int left, int right)
 {
-    int limit = 8; // 8 is a random number
+    int limit = 10;
     
+    // use insert sort if the list is small
     if (right - left + 1 <= limit)
     {
         insertSort(list, left, right);
         return;
     }
 
+    // else use merge sort recursively
     int middle = (left + right) / 2;
     mergeSort(list, left, middle);
     mergeSort(list, middle + 1, right);
@@ -66,7 +120,7 @@ void sort(std::vector<int> &list)
 
 int main()
 {
-    int numbers_array[] = {8, 2, 5, 3, 4, 7, 6, 1};
+    int numbers_array[] = {8, 2, 5, 3, 4, 7, 6, 1, 9, 0, 10};
     std::vector<int> array(numbers_array, numbers_array + sizeof(numbers_array) / sizeof(int));
 
     print(array, "Before sort : ");
